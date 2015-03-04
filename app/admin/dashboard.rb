d958caaebae8a02567057e5a48b386665a3f913b
lvl0nax <lvl0nax@gmail.com>
2015-03-05 00:28:26 +0300

    cosmetic fix

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,16 +1,5 @@
 ActiveAdmin.register_page "Dashboard" do
   content do
     "You have #{ Subscription.count } subscribers."
-
-    table do
-      thead do
-        tr do
-          %w[Article comments_count].each &method(:th)
-        end
-      end
-      tbody do
-        Article.last(10)
-      end
-    end
   end
 end
